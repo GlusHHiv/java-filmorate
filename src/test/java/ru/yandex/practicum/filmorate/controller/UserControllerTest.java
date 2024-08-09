b481886f976cfe6944d7d6e647b783dff39e6690
IvanGlush <poliantsevivan@gmail.com>
2024-08-09 02:55:04 +0200

    fix: JUnit

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -7,22 +7,6 @@ import ru.yandex.practicum.filmorate.model.User;
 
 public class UserControllerTest {
     UserController userController = new UserController();
-    
-    @Test
-    public void emailValidationTest() {
-        Assertions.assertThrows(ValidationException.class,
-                () -> userController.create(new User(0L,
-                        "",
-                        "null",
-                        "null",
-                        "1999-09-09")));
-        Assertions.assertThrows(ValidationException.class,
-                () -> userController.create(new User(0L,
-                        "testwithoutAt",
-                        "null",
-                        "null",
-                        "1999-09-09")));
-    }
 
     @Test
     public void birthdayValidationTest() {
@@ -46,6 +30,22 @@ public class UserControllerTest {
                 () -> userController.create(new User(0L,
                         "null@gmail.com",
                         null,
+                        "null",
+                        "1999-09-09")));
+    }
+
+    @Test
+    public void emailValidationTest() {
+        Assertions.assertThrows(ValidationException.class,
+                () -> userController.create(new User(0L,
+                        "",
+                        "null",
+                        "null",
+                        "1999-09-09")));
+        Assertions.assertThrows(ValidationException.class,
+                () -> userController.create(new User(0L,
+                        "testwithoutAt",
+                        "null",
                         "null",
                         "1999-09-09")));
     }
